f2e2253a761127991f0da418f09e12ddaee3e5e9
PythonCoder55 <edwardbarchv@gmail.com>
2023-06-01 12:54:10 -0400

    end of task 3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int balance = 5000;
+        double interest;
+
+        interest = balance * .17;
+        System.out.println("The interest after one month is " + interest);
+
+        interest = (balance + interest) * .17;
+        System.out.println("The interest after two months is " + interest);
     }
 }
